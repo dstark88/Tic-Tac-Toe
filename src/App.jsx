9fb947325c09cfd49f9f0b3a4f1644975e29c499
Denise Stark <starkwalker88@gmail.com>
2024-02-08 16:36:11 -0700

    finished tic tac toe

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,74 @@
+import { useState } from "react";
+import {
+  PLAYERS,
+  deriveActivePlayer,
+  deriveGameBoard,
+  deriveWinner,
+} from "./Utils.jsx";
+import Player from "./components/Player.jsx";
+import GameBoard from "./components/GameBoard.jsx";
+import GameOver from "./components/GameOver.jsx";
 
 function App() {
-  
+  const [players, setPlayers] = useState(PLAYERS);
+  const [gameTurns, setGameTurns] = useState([]);
+
+  const activePlayer = deriveActivePlayer(gameTurns);
+  const gameBoard = deriveGameBoard(gameTurns);
+  const winner = deriveWinner(gameBoard, players);
+  const hasDraw = gameTurns.length === 9 && !winner;
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameTurns((prevTurns) => {
+      const currentPlayer = deriveActivePlayer(prevTurns);
+
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+
+      return updatedTurns;
+    });
+  }
+
+  function handleRestart() {
+    setGameTurns([]);
+  }
+
+  function handlePlayerNameChange(symbol, newName) {
+    setPlayers((prevPlayers) => {
+      return {
+        ...prevPlayers,
+        [symbol]: newName,
+      };
+    });
+  }
 
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <header id="header">Tic Tac Toe</header>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName={PLAYERS.X}
+            symbol="X"
+            isActive={activePlayer === "X"}
+            onChangeName={handlePlayerNameChange}
+          />
+          <Player
+            initialName={PLAYERS.O}
+            symbol="O"
+            isActive={activePlayer === "O"}
+            onChangeName={handlePlayerNameChange}
+          />
+        </ol>
+        {(winner || hasDraw) && (
+          <GameOver winner={winner} onRestart={handleRestart} />
+        )}
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
